7c051be0a9e5dc08e500345092a9c73e8ce2f392
Samueljesulowo <samueljesulowo@gmail.com>
2022-03-15 09:25:55 +0000

    compile error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Write a function that prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0
+ * print_alphabet - write a function that prints the alphabet in lowercase
  */
 void print_alphabet(void)
 {
@@ -18,6 +16,4 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
-
-	return (0);
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,12 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowecase
  * followed by a new line
- * Return: Always 0
  */
 
 void print_alphabet_x10(void)
 {
 	char ch;
-	int 1;
+	int i;
 
 	i = 0;
 
