61a34be00ded02e157f5705ee7a89ee3be044cbd
Samueljesulowo <samueljesulowo@gmail.com>
2022-03-15 12:24:17 +0000

    c is fun

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - functions that adds two integers
+ * @a: int to be added
+ * @b: int to be added
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
