c30718a0e51fb89b2c244704fac169f8f691d104
Samueljesulowo <samueljesulowo@gmail.com>
2022-03-15 12:55:18 +0000

    c is fun

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
+
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
@@ -21,6 +22,7 @@ void times_table(void)
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
