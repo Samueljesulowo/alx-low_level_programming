ecb8d1abab1a4ad7f0d40e92f6829879631ad258
Samueljesulowo <samueljesulowo@gmail.com>
2022-03-22 19:48:30 -0700

    my commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void positive_or_negative(int i);
+/**
+ * main - test the functions that print if an integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i =0;
+	positive_or_negative(i);
+
+	return (0);
+}
