217a18e277183b0dba9af5cb0096570e3d7660b4
Samueljesulowo <samueljesulowo@gmail.com>
2022-03-22 20:05:20 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,20 +3,22 @@
 /**
  * main - cause an infinite loop
  * Return: 0
- */
+ **/
+
 int main(void)
 {
 	int i;
+
 	printf("Infinte loop incoming :(\n");
 
 		/*
-		 * i =0;
+		 * i = 0;
 		 * while (i < 10)
 		 * {
 		 * putchar(i);
 		 * putchar(i);
 		 * }
-		 */
+		 **/
 		printf("Infinte loop avoided! \\o/\n");
 
 		return (0);
